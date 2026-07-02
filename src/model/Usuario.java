ad5ef76a47e887c6eba3d0234cbbc665933666ce
João Vitor <almeidaoliveira9697@gmail.com>
2026-07-01 22:06:49 -0300

    feat: adiciona a classe Usuario

---
+++ src/model/Usuario.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void consultarHistorico() {
+        System.out.println("Consultando histórico...");
+    }
+
+    public void buscarLivro() {
+        System.out.println("Buscando livro...");
+    }
+}
