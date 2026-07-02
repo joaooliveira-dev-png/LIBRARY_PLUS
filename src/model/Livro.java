8d0849af898291f0c4f4eb6c7014fe83ac80cf76
João Vitor <almeidaoliveira9697@gmail.com>
2026-07-01 22:07:35 -0300

    feat: adiciona a classe Livro

---
+++ src/model/Livro.java
@@ -0,0 +1,74 @@
+package model;
+
+public class Livro {
+    
+    private int id;
+    private String titulo;
+    private String autor;
+    private String editora;
+    private int quantidade;
+
+    public Livro() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void diminuirQuantidade() {
+        if (quantidade > 0) {
+            quantidade--;
+        }
+    }
+
+    public void aumentarQuantidade() {
+        quantidade++;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", editora='" + editora + '\'' +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
